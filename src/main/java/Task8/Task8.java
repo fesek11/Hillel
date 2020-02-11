8501eb4c6bb3f6e5407eb9af27b1b49e143e0dfc
Denis Fesenko <dfesek1@gmail.com>
2020-02-12 00:15:11 +0200

    добавил  мебель в мейн

--- src/main/java/Task8/Task8.java
+++ src/main/java/Task8/Task8.java
@@ -1,7 +1,18 @@
 package Task8;
 
+
 public class Task8 {
     public static void main(String[] args) {
+        Task3House task3House = new Task3House();
+        task3House.chair.legs = 3;
+        task3House.chair.name = "new chair";
+        task3House.chair.weight = 2.3;
+        task3House.penal.legs = 4;
+        task3House.penal.name ="penal";
+        task3House.table.weight = 2.3;
+        task3House.table.legs = 1;
+        task3House.table.name = "new table";
 
+     //   System.out.println(task3House.toString());
     }
 }
